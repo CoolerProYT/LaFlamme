6722331af232c683aefcf5d43bfaa9f9fedcd31e
CoolerProYT <veronlam1818@gmail.com>
2024-06-21 10:10:29 +0800

    Changed promotion popup style

--- resources/views/livewire/public/promotion.blade.php
+++ resources/views/livewire/public/promotion.blade.php
@@ -1,16 +1,18 @@
 <div>
     @if($enabled && $flag)
         <div class="pop-up d-flex justify-content-center align-items-center">
-            <div class="bg-dark text-light rounded p-3 col-3">
-                <div class="text-end col-12">
-                    <span class="h2 pointer" wire:click="updateFlag(false)">X</span>
+            <div class="bg-dark text-light col-xl-3 col-10 col-md-8 col-lg-6 promotion">
+                <div class="text-end col-12 py-2 px-3 close-promotion">
+                    <span class="h2 pointer outline" wire:click="updateFlag(false)">X</span>
                 </div>
-                <div class="my-3">
+                <div class="">
                     <img class="col-12" src="{{ asset('storage/public/promotion/' . $image) }}">
                 </div>
-                <div class="text-center">
-                    <pre style="white-space: break-spaces;font-family: Arial">{{ $description }}</pre>
-                </div>
+                @if($description != '')
+                    <div class="text-center pt-2">
+                        <pre style="white-space: break-spaces;font-family: Arial">{{ $description }}</pre>
+                    </div>
+                @endif
             </div>
         </div>
     @endif
